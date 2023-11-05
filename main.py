9ba13fee6edf64abafa1760ebc00d467c1702250
olenapolianska <141331552+olenapolianska@users.noreply.github.com>
2023-11-05 18:28:14 +0200

    les2

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
 from PyQt5.QtWidgets import *
-
 app = QApplication([ ])
+
+
 window = QWidget()
 folder = QPushButton("Папка")
 list = QListWidget()
 photo = QLabel("фото")
-QPushButton
+left = QPushButton("Вліво")
+right = QPushButton("Вправо")
+mirror = QPushButton("Дзеркало")
+sharpness = QPushButton("Різкість")
+chb = QPushButton("Ч/Б")
 
 mainline = QHBoxLayout()
 line = QVBoxLayout()
 line2 = QVBoxLayout()
 line3 = QHBoxLayout()
-line2.addLayout(line3)
 mainline.addLayout(line)
 mainline.addLayout(line2)
 
 line.addWidget(folder)
 line.addWidget(list)
 line2.addWidget(photo)
+line2.addLayout(line3)
+line3.addWidget(left)
+line3.addWidget(right)
+line3.addWidget(mirror)
+line3.addWidget(sharpness)
+line3.addWidget(chb)
 
+app.setStyleSheet("""
+        QWidget{
+            background:#EAD4FD;
+        }
+        QPushButton
+        {
 
+            background-color:#E7D4FD;
+            font-size: 11px;
+            color: grey;
+            font-family: Courier New;
+            border-style: dotted;
+            border-width: 1px;
+            border-color: black;
+            border-radius: 4px;
+            
+
+            }
+        """)
 
 window.setLayout(mainline)
 window.show()

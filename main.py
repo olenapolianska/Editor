d9ea17a3d5cd5b574974a3580e8da174a69505aa
olenapolianska <141331552+olenapolianska@users.noreply.github.com>
2023-11-12 18:30:59 +0200

    les3

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os
+
+from PIL import Image
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
+
 app = QApplication([ ])
 
 
@@ -48,7 +54,61 @@ app.setStyleSheet("""
 
             }
         """)
+class WorkPhoto:
+    def __init__(self):
+        self.image = None
+        self.folder = None
+        self.filname = None
+    def load(self):
+        imagePath = os.path.join(self.folder, self.filename)
+        self.image = Image.open(imagePath)
 
+    def showImage(self):
+        pixel = pil2pixmap(self.image)
+        pixel = pixel.scaled(800, 600, Qt.KeepAspectRatio)
+        photo.setPixmap(pixel)
+    def rotate_left(self):
+        self.image = self.image.transpose(Image.ROTATE_90)
+        self.showImage()
+    def rotate_right(self):
+        self.image = self.image.transpose(Image.ROTATE_270)
+        self.showImage()
+
+work = WorkPhoto()
+left.clicked.connect(work.rotate_left)
+right.clicked.connect(work.rotate_right)
+def open_folder():
+    work.folder = QFileDialog.getExistingDirectory()
+    files = os.listdir(work.folder)
+    list.clear()
+    list.addItems(files)
+def rotate_left(self):
+    self.image = self.image.transpose(Image.ROTATE_90)
+    self.showImage()
+
+
+def showChosenImage():
+    work.filename = list.currentItem().text()
+    work.load()
+    work.showImage()
+def pil2pixmap(im):
+    if im.mode == "RGB":
+        r, g, b = im.split()
+        im = Image.merge("RGB", (b, g, r))
+    elif im.mode == "RGBA":
+        r, g, b, a = im.split()
+        im = Image.merge("RGBA", (b, g, r, a))
+    elif im.mode == "L":
+        im = im.convert("RGBA")
+    im2 = im.convert("RGBA")
+    data = im2.tobytes("raw", "RGBA")
+    qim = QImage(data, im.size[0], im.size[1], QImage.Format_ARGB32)
+    pixmap = QPixmap.fromImage(qim)
+    return pixmap
+
+
+list.currentRowChanged.connect(showChosenImage)
+folder.clicked.connect(open_folder)
 window.setLayout(mainline)
 window.show()
 app.exec()
